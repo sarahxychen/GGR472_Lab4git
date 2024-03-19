1e7c93f3e10f8a8da3db6d9987d17cbc9f41c369
sarahxychen <sarahxy.chen@mail.utoronto.ca>
2024-03-19 15:01:52 -0400

    Collision data density

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lab4git
  
+ Measuring the crash collision frequency of Toronto CMA using hexgrid density counts. 
+
+ Eplore the map by
+    a. Toggling the hex count density layer
+    b. Toggling the point count layer
--- script.js
+++ script.js
@@ -48,12 +48,91 @@ map.on('load', () => {
         'type': 'circle',
         'source': 'pedbike_collision',
         'paint': {
-            'circle-radius': 5,
-            'circle-color': 'blue'
+            'circle-radius': 4,
+            'circle-color': '#010c1c'
         }
     });
     
 });
+
+
+//map load event handler and create bounding box envelope
+map.on('load', () => {
+    let bbox = turf.envelope(colgeojson);
+    let bboxscaled = turf.transformScale(bbox, 1.50);
+
+    // put the resulting envelope in a geojson format FeatureCollection
+    bboxgeojson = {
+        "type": "FeatureCollection",
+        "features": [bboxscaled]
+    };
+    
+    //Create turf hexgrid using coordinates from bounding box array 
+    let bboxcoords= [-79.621974,43.590289,-79.122974,43.837935]
+         // bboxcoords = [min x, min y, max x, max y] -> find using console.log(bboxgeojson) in console log
+    let hexgeojson = turf.hexGrid(bboxcoords, 0.6, { units: 'kilometers' }) //increase hex cell to improve pattern visbility for crash counts
+
+
+    //Step 4: AGGREGATE COLLISIONS BY HEXGRID
+    //Turf collect function -> collect properties form the point layer by hex polygons
+    let collishex= turf.collect(hexgeojson, colgeojson, '_id', 'values'); 
+
+    //count # of features inside each hexagon, and identify maximum value 
+    let maxcollis = 0;
+
+    collishex.features.forEach((feature) => {
+        feature.properties.COUNT = feature.properties.values.length
+        if (feature.properties.COUNT > maxcollis) {
+         console.log(feature);
+         maxcollis = feature.properties.COUNT
+      }
+    });
+
+// Step 5: FINALIZE YOUR WEB MAP
+//Link hex density to button and display on map:
+document.getElementById('bbox').addEventListener('click', () => {
+
+    // stylize data after adding onto map 
+    map.addSource('collis-hex', {
+        type: 'geojson',
+        data: hexgeojson  // use hexgrid as source
+    });
+
+    map.addLayer({
+        'id': "collis-hex-fill",
+        'type': "fill",
+        'source': "collis-hex",
+        'paint': {
+            'fill-color': [
+                'step', //style data output
+                ['get','COUNT'],
+                '#a9c9f5', //count 0-4 density
+                5, '#7faef0', //count 5-9 density
+                10, '#608dcc', //count 10-19 density
+                20, '#3b66a3', //count 20-29 density
+                30, '#27508a', // count 30-39 density
+                40, '#163e75', // count 40-71
+                72, '#042757', //max count: 72
+            ],
+            'fill-opacity': 0.8,
+            'fill-outline-color': "white"
+        }
+    });
+
+    document.getElementById('bbox').disabled = true; // disable button after click
+    }); 
+
+    //add popup feature
+    map.on('click', 'collis-hex-fill',(e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML("<b>Collision count:</b>" + e.features[0].properties.COUNT)
+            .addTo(map);
+    })
+
+});
+
+
 
 /*--------------------------------------------------------------------
     Step 3: CREATE BOUNDING BOX AND HEXGRID
@@ -63,22 +142,8 @@ map.on('load', () => {
 //      Access and store the bounding box coordinates as an array variable
 //      Use bounding box coordinates as argument in the turf hexgrid function
 
-//map load event handler and create bounding box envelope
-map.on('load', () => {
-    let bbox = turf.envelope(colgeojson);
-    let bboxscaled = turf.transformScale(bbox, 1.10);
-
-    // put the resulting envelope in a geojson format FeatureCollection
-    bboxgeojson = {
-        "type": "FeatureCollection",
-        "features": [bboxscaled]
-    };
-});
-
 // //Link boundring box to button:
-
 // document.getElementById('bbox').addEventListener('click', () => {
-
 //     // add the bounding box to the map to view
 //     map.addSource('collis-bbox', {
 //         "type": "geojson",
@@ -101,39 +166,37 @@ map.on('load', () => {
 
 //Create turf hexgrid using coordinates from bounding box array 
 // bboxcoords = [min x, min y, max x, max y] -> find using console.log(bboxgeojson) in console log
-
-let bboxcoords= [-79.621974,43.590289,-79.122974,43.837935]
-
 // let bboxcoords = [bboxgeojson.geometry.coordinates[0][0][0], // min x coordinates
 //                  bboxgeojson.geometry.coordinates[0][0][1], // min y coordinates
 //                  bboxgeojson.geometry.coordinates[0][2][0], // max x coordinates
 //                  bboxgeojson.geometry.coordinates[0][2][1]]; // max y coordinates
 
-    let hexgeojson = turf.hexGrid(bboxcoords, 0.5, { units: 'kilometers' });
+// let bboxcoords= [-79.621974,43.590289,-79.122974,43.837935]
+// let hexgeojson = turf.hexGrid(bboxcoords, 0.5, { units: 'kilometers' });
 
-//Link hex boundring box to button:
+// //Link hex boundring box to button:
 
-document.getElementById('bbox').addEventListener('click', () => {
+// document.getElementById('bbox').addEventListener('click', () => {
 
-    // add the bounding box to the map to view
-    map.addSource('collis-bbox', {
-        "type": "geojson",
-        "data": hexgeojson  // use bbox geojson bounding box as source
-    });
+//     // add the bounding box to the map to view
+//     map.addSource('collis-bbox', {
+//         "type": "geojson",
+//         "data": hexgeojson  // use bbox geojson bounding box as source
+//     });
 
-    map.addLayer({
-        "id": "collisEnvelope",
-        "type": "fill",
-        "source": "collis-bbox",
-        "paint": {
-            'fill-color': "blue",
-            'fill-opacity': 0.5,
-            'fill-outline-color': "black"
-        }
-    });
+//     map.addLayer({
+//         "id": "collisEnvelope",
+//         "type": "fill",
+//         "source": "collis-bbox",
+//         "paint": {
+//             'fill-color': "blue",
+//             'fill-opacity': 0.5,
+//             'fill-outline-color': "black"
+//         }
+//     });
 
-    document.getElementById('bbox').disabled = true; // disable button after click
-}); 
+//     document.getElementById('bbox').disabled = true; // disable button after click
+// }); 
 
 /*--------------------------------------------------------------------
 Step 4: AGGREGATE COLLISIONS BY HEXGRID
@@ -141,7 +204,79 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 //HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
 //      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
 
+//Turf collect function -> collect properties form the point layer by hex polygons
+// let collishex= turf.collect(hexgeojson,colgeojson,'_id','values'); 
 
+// //count # of features inside each hexagon, and identify maximum value 
+// let maxcollis = 0;
+
+// collishex.features.forEach((feature) => {
+//     feature.properties.COUNT = feature.properties.values.length
+//     if (feature.properties.COUNT > maxcollis) {
+//         console.log(feature);
+//         maxcollis = feature.properties.COUNT
+//     }
+// });
+
+// //Link hex boundring box to button:
+
+// document.getElementById('bbox').addEventListener('click', () => {
+
+//     // add the bounding box to the map to view
+//     map.addSource('collis-bbox', {
+//         "type": "geojson",
+//         "data": hexgeojson  // use bbox geojson bounding box as source
+//     });
+
+//     map.addLayer({
+//         "id": "collisEnvelope",
+//         "type": "fill",
+//         "source": "collis-bbox",
+//         "paint": {
+//             'fill-color': [
+//                 'step', //style data output
+//                 ['get','COUNT'],
+//                 '#800026',
+//                 10, 'bd0026',
+//                 25, 'e31a1c'
+//             ],
+//             'fill-opacity': 0.5,
+//             'fill-outline-color': "white"
+//         }
+//     });
+
+//     document.getElementById('bbox').disabled = true; // disable button after click
+// }); 
+
+// document.getElementById('bboxdensity').addEventListener('click', () => {
+
+//     // add the bounding box to the map to view
+//     map.addSource('collis-hex', {
+//         "type": "geojson",
+//         "data": hexgeojson  // use bbox geojson bounding box as source
+//     });
+
+//     map.addLayer({
+//         "id": "collis-hex-fill",
+//         "type": "fill",
+//         "source": "collis-hex",
+//         "paint": {
+//             'fill-color': [
+//                 'step', //style data output
+//                 ['get','COUNT'],
+//                 '#800026',
+//                 10, 'bd0026',
+//                 25, 'e31a1c'
+//             ],
+//             'fill-opacity': 0.5,
+//             'fill-outline-color': "white"
+//         }
+//     });
+
+//     document.getElementById('bboxdensity').disabled = true; // disable button after click
+// }); 
+
+//add and display data on map
 
 // /*--------------------------------------------------------------------
 // Step 5: FINALIZE YOUR WEB MAP
@@ -152,5 +287,20 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 //        - The COUNT attribute
 //        - The maximum number of collisions found in a hexagon
 //      Add a legend and additional functionality including pop-up windows
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
